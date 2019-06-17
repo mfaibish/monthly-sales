2fd7c552020f80e3696f6cf98c92bc7616d776ac
Melanie Faibish <mlf398@nyu.edu>
2019-06-17 19:18:52 -0400

    print month and date for filename

--- reporter.py
+++ reporter.py
@@ -11,7 +11,7 @@ def to_usd(my_price):
 if __name__ == "__main__":
     print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
 
-    sales_filepath = os.path.join(os.path.dirname(__file__), "data", "sales-201902.csv")
+    sales_filepath = os.path.join(os.path.dirname(__file__), "data", "sales-201903.csv")
 
 # IMPORT CSV
     rows = []
@@ -23,9 +23,39 @@ if __name__ == "__main__":
     sales_price = [float(row["sales price"]) for row in rows]
 
     total_sales = sum(sales_price)
-    print(to_usd(total_sales))
+
+    months = [
+    {"id":"01", "month": "January"},
+    {"id":"02", "month": "February"},
+    {"id":"03", "month": "March"},
+    {"id":"04", "month": "April"},
+    {"id":"05", "month": "May"},
+    {"id":"06", "month": "June"},
+    {"id":"07", "month": "July"},
+    {"id":"08", "month": "August"},
+    {"id":"09", "month": "September"},
+    {"id":"10", "month": "October"},
+    {"id":"11", "month": "November"},
+    {"id":"12", "month": "December"}
+    ]
+    
+    year = sales_filepath[11:-6]
+    month = sales_filepath[15:-4]
+    
+    month_name = [m for m in months if m["id"] == month]
+    name = month_name[0]
 
     
+    print("-------------------------")
+    print("SALES REPORT (" + name["month"] + " " + year + ")")
+    print("-------------------------")
+    print("TOTAL SALES: " + str(to_usd(total_sales)))
+    print("-------------------------")
+
+
+ 
+
+
 
    
 
