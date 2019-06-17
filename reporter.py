ae1ecb1953d9f14ab08165b2e990a38312cd35a3
Melanie Faibish <mlf398@nyu.edu>
2019-06-16 22:40:57 -0400

    Succesfully imported csv file

--- reporter.py
+++ reporter.py
@@ -1,3 +1,16 @@
 # reporter.py
+import pandas
+import os
 
-print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
+if __name__ == "__main__":
+   print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
+
+   sales_filepath = os.path.join(os.path.dirname(__file__), "data", "sales-201902.csv")
+
+   df = pandas.read_csv(sales_filepath)
+
+   print(df)
+   
+
+
+
